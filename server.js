80dae2a1a86f05f5189263e48fb974c0cdcbca09
Ervin Szilagyi <ernyoke19930115@gmail.com>
2020-04-12 18:42:08 +0300

    Implemented user registration.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const connectDB = require('./config/db');
 const app = express();
 connectDB();
 
+app.use(express.json({extended: false}));
+
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
