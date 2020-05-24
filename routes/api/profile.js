6d9e1179161d5a4924909cd5b35c2d9bfc0a79e9
Ernyoke <ernyoke19930115@gmail.com>
2020-05-24 22:22:34 +0300

    Added github repositories to profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
+const chalk = require('chalk');
 const { check, validationResult } = require('express-validator');
 
 const auth = require('../../middleware/auth');
@@ -298,6 +301,37 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     } catch (err) {
         console.error(err);
         return res.status(500).send('Server error');
+    }
+});
+
+// @route GET api/profile/github/:username
+// @desc Get user repos from Github
+// @access Public
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=creted:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: {
+                'user-agent': 'node.js'
+            }
+        };
+        
+        request(options, (error, response, body) => {
+            if (error) {
+                console.log(chalk.red(error));
+            }
+
+            if (response.statusCode !== 200) {
+                return res.status(404).json({
+                    msg: 'No Github profile found!'
+                });
+            }
+
+            return res.json(JSON.parse(body));
+        });
+    } catch (err) {
+        console.error(chalk.red(err))
     }
 });
 
