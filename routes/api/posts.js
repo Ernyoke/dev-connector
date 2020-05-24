c88350451de0de5067a614a955faf3cc104d5745
Ernyoke <ernyoke19930115@gmail.com>
2020-05-24 23:05:06 +0300

    Implemented get posts.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,9 +9,42 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 // @route GET api/posts
-// @desc Test route
-// @access Public
-router.get('/', (req, res) => res.send('Posts route!'));
+// @desc Get all posts
+// @access Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({
+            date: -1
+        });
+        return res.json(posts);
+    } catch (err) {
+        console.error(chalk.red(err));
+        return res.status(500).send('Server error!');
+    }
+});
+
+// @route GET api/posts/:id
+// @desc Get post by id
+// @access Private
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({
+                msg: 'Post not found!'
+            });
+        }
+        return res.json(post);
+    } catch (err) {
+        if (err.name === 'CastError') {
+            return res.status(404).json({
+                msg: 'Post not found!'
+            });
+        }
+        console.error(chalk.red(err));
+        return res.status(500).send('Server error!');
+    }
+});
 
 
 // @route POST api/posts
